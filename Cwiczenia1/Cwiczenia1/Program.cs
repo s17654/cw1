9652369345e0de5356454344a2f4adde70701279
Łukasz Markowski <s17654@pjwstk.edu.pl>
2020-02-29 20:58:05 +0100

    pierwszy projekt

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode) //2xx
+            {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+
+
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
